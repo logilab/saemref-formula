8d5c2027f163d5f6981d0dba7f7c628168c0b6a5
Philippe Pepiot <philippe.pepiot@logilab.fr>
2016-06-24 17:28:25 +0200

    test: look only the major version of saem cube

--- test/test_saemref.py
+++ test/test_saemref.py
@@ -10,7 +10,7 @@ wait_saemref_started = pytest.mark.usefixtures("_wait_saemref_started")
 def test_package_saem_ref(Package):
     cube = Package("cubicweb-saem-ref")
     assert cube.is_installed
-    assert cube.version.startswith("0.12.0")
+    assert cube.version.startswith("0.12.")
 
 
 def test_package_cubicweb(Package, SystemInfo):

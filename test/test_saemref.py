e8f04dcfb744b5e2d3487924cb0c937728225898
Denis Laxalde <denis.laxalde@logilab.fr>
2016-11-04 15:51:34 +0100

    test: bump cubicweb version to 3.24.0

--- test/test_saemref.py
+++ test/test_saemref.py
@@ -34,8 +34,8 @@ def test_package_cubicweb(Package, SystemInfo):
 
     cubicweb = Package(name)
     assert cubicweb.is_installed
-    assert cubicweb.version.startswith("3.23")
-    assert map(int, cubicweb.version.split('.')) >= [3, 23, 1]
+    assert cubicweb.version.startswith("3.24")
+    assert map(int, cubicweb.version.split('.')) >= [3, 24, 0]
 
 
 @pytest.mark.parametrize("name, version", [

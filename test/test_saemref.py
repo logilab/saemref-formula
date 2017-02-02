6d054cc67df30d66fa21f5695f2f5059a2064fd8
Sylvain Thenault <sylvain.thenault@logilab.fr>
2017-02-02 11:49:46 +0100

    Expect saem-ref 0.14.2

--- test/test_saemref.py
+++ test/test_saemref.py
@@ -24,11 +24,11 @@ def test_pip_packages(PipPackage):
     packages = PipPackage.get_packages(pip_path='/home/saemref/venv/bin/pip')
     assert packages['cubicweb']['version'].startswith("3.24")
     assert map(int, packages['cubicweb']['version'].split('.')) >= [3, 24, 5]
-    assert packages['cubicweb-saem-ref']['version'].startswith("0.14.1")
+    assert packages['cubicweb-saem-ref']['version'].startswith("0.14.2")
 
 
 @pytest.mark.parametrize("name, version", [
-    ("cubicweb_saem_ref", "0.14.1"),
+    ("cubicweb_saem_ref", "0.14.2"),
 ])
 def test_devinstall(Command, name, version):
     cmd = "/home/saemref/venv/bin/cubicweb-ctl list cubes"

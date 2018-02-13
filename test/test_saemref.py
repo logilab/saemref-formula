582338e54537ee94add99edc72ab983fbd11fa66
Denis Laxalde <denis@laxalde.org>
2018-02-13 16:12:12 +0100

    Bump expected version to 0.17.3

--- test/test_saemref.py
+++ test/test_saemref.py
@@ -9,7 +9,7 @@ import pytest
 
 
 DATA_DIR = path.join(path.abspath(path.dirname(__file__)), "data")
-EXPECTED_SAEM_VERSION = "0.17.2"
+EXPECTED_SAEM_VERSION = "0.17.3"
 
 
 wait_supervisord_started = pytest.mark.usefixtures("_wait_supervisord_started")
